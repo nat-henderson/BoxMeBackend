f2966031e6eef22cdf4da800378f46878633bfb1
Vaibhav Arora <vaibhavarora.nitt@gmail.com>
2012-07-21 18:53:29 -0700

    Adding Interface of Storage Provider with copy Files functionality

--- src/hackathon/boxme/StorageProvider.java
+++ src/hackathon/boxme/StorageProvider.java
@@ -6,10 +6,27 @@ import java.util.List;
 
 public interface StorageProvider {
 	
-	public void putFile(FileInputStream inputStream, List<String> credentials);
+	/*
+	 * Copy the file input stream to list of users given their credentials
+	 */
+	public void putFile(FileInputStream inputStream, List<String> receiverCredentials);
 	
-	public FileOutputStream getFile(String key, String credentials);
+	/*
+	 *  Get the file output stream to list of users given sender credentials
+	 *  fileKey is an unique fileId 
+	 */
+	public FileOutputStream getFile(String fileKey, String senderCredentials);
 	
+	
+	/*
+	 *  copyFiles between a sender and a receiver
+	 *  fileKey is an unique fileId 
+	 */
+	public boolean copyFile(String fileKey, String senderCredentials, List<String> receiverCredentials);
+	
+	/*
+	 * Return a list of files given credentials
+	 */
 	public List<String> listAllFiles(String credentials);
 	
 }
