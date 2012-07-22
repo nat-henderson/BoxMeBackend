5f5679bdd06568c5f864ebe3d7a7277ccd989410
Nathan McKinley <ndm25@case.edu>
2012-07-22 04:26:12 -0700

    spaces to slashes
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

--- src/hackathon/boxme/S3StorageProvider.java
+++ src/hackathon/boxme/S3StorageProvider.java
@@ -17,8 +17,8 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 public class S3StorageProvider implements StorageProvider {
 	
 	public AmazonS3Client getClient(String credentials) {
-		final String secret = credentials.split(" ")[1];
-		final String access = credentials.split(" ")[0];
+		final String secret = credentials.split("/")[1];
+		final String access = credentials.split("/")[0];
 		return new AmazonS3Client(new AWSCredentials() {
 			
 			@Override
@@ -37,8 +37,8 @@ public class S3StorageProvider implements StorageProvider {
 	public void putFile(String fileKey, FileCopyStream stream,
 			String receiverCredentials) {
 		AmazonS3Client client = getClient(receiverCredentials);
-		String bucket = fileKey.split(" ")[0];
-		String filename = fileKey.split(" ")[1];
+		String bucket = fileKey.split("/")[0];
+		String filename = fileKey.split("/")[1];
 		new Thread(new S3PutFileRequest(bucket, filename, stream, client)).start();
 	}
 
@@ -46,7 +46,7 @@ public class S3StorageProvider implements StorageProvider {
 	public FileCopyStream getFile(String fileKey, String senderCredentials) {
 		FileCopyStream stream = new FileCopyStream();
 		AmazonS3Client client = getClient(senderCredentials);
-		S3Object obj = client.getObject(fileKey.split(" ")[0], fileKey.split(" ")[1]);
+		S3Object obj = client.getObject(fileKey.split("/")[0], fileKey.split("/")[1]);
 		stream.setInputStream(obj.getObjectContent());
 		stream.setSize(obj.getObjectMetadata().getContentLength());
 		return stream;
@@ -60,11 +60,11 @@ public class S3StorageProvider implements StorageProvider {
 		AmazonS3Client client = getClient(credentials);
 		String bucket = null;
 		String fileName = null;
-		if (path.split(" ").length > 1) {
-			bucket = path.split(" ")[0];
+		if (path.split("/").length > 1) {
+			bucket = path.split("/")[0];
 			fileName = "";
-			for (int i = 1; i < path.split(" ").length; i++) {
-				fileName += path.split(" ")[i];
+			for (int i = 1; i < path.split("/").length; i++) {
+				fileName += path.split("/")[i];
 			}
 		} else if (path.length() > 0) {
 			bucket = path;
@@ -72,15 +72,13 @@ public class S3StorageProvider implements StorageProvider {
 		List<Bucket> buckets = client.listBuckets();
 		for (Bucket b : buckets){
 			if (bucket == null || b.getName().startsWith(bucket)) {
-				ObjectListing ol = client.listObjects(path.split(" ")[0], path.split(" ")[1]);
+				ObjectListing ol = client.listObjects(bucket, fileName);
 				do {
 					for (S3ObjectSummary sum : ol.getObjectSummaries()) {
-						if (fileName == null || sum.getKey().startsWith(fileName)) {
-							if (sum.getSize() == 0) {
-								dirs.add(sum.getBucketName() + " " + sum.getKey());
-							} else {
-								files.add(sum.getBucketName() + " " + sum.getKey());
-							}
+						if (sum.getSize() == 0) {
+							dirs.add(sum.getBucketName() + "/" + sum.getKey());
+						} else {
+							files.add(sum.getBucketName() + "/" + sum.getKey());
 						}
 					}
 					ol = client.listNextBatchOfObjects(ol);
