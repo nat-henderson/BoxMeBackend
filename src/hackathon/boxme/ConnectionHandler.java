8d63313abcbc4744186566904302368dc221e428
Nathan McKinley <ndm25@case.edu>
2012-07-22 07:36:20 -0700

    debug2
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

--- src/hackathon/boxme/ConnectionHandler.java
+++ src/hackathon/boxme/ConnectionHandler.java
@@ -31,12 +31,13 @@ public class ConnectionHandler implements Runnable {
 		BoxMeRequest request = null;
 		System.out.println(clientSocket.isClosed());
 		try {
-			StringBuilder sb = new StringBuilder();
+			StringBuilder sb = new StringBuilder("");
 			String string = "initialSet";
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
 			while (!string.equals("")) {
 				System.out.println(string);
-				string = new BufferedReader(new InputStreamReader(
-						clientSocket.getInputStream())).readLine();
+				string = reader.readLine();
 				sb.append(string);
 			}
 
